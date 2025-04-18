2d3d5cc40f3200635abb927cdf3054c784beada3
Sudip Das <143950684+sudip547@users.noreply.github.com>
2025-04-18 19:37:29 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-uvicorn backend.main:app --host 0.0.0.0 --port 10000
+# This script is used to start the FastAPI app
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Start the app
+uvicorn main:app --host 0.0.0.0 --port $PORT
+
