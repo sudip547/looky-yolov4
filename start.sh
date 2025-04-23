128e4f99adc2f8b71359de7f2f629bbdd3c31751
Sudip Das <143950684+sudip547@users.noreply.github.com>
2025-04-23 23:30:21 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Download weights if not already present
-if [ ! -f backend/yolov4.weights ]; then
-  echo "Downloading yolov4.weights from Google Drive..."
-  gdown --id 1cyveqbqjkI5XDhiN3nN4WGa4kTbPFnRt -O backend/yolov4.weights
+# Download weights if not present
+if [ ! -f backend/yolov4-tiny.weights ]; then
+  echo "Downloading yolov4-tiny.weights from Google Drive..."
+  gdown --id 18YmokC8RyTIKA_FXDU_lY3zMC8dSWaKv -O backend/yolov4-tiny.weights
 fi
 
-# Start FastAPI app
-echo "Starting FastAPI server..."
+# Start FastAPI server
 uvicorn backend.main:app --host 0.0.0.0 --port 10000
+
